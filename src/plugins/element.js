86293f4911d95878443b5cfa83ebd3b2d5170f4e
emptymzh <1718294983@qq.com>
2021-08-27 21:46:58 +0800

    完成了订单管理功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,6 @@
 //如果你一开始配置element插件时选了全局导入就不用一个个写了
 import Vue from 'vue'
-import { Button, Step } from 'element-ui'
+import { Button, Step} from 'element-ui'
 import { Form,FormItem } from 'element-ui'
 import { Input } from 'element-ui'
 import { Container, Header, Aside, Main } from 'element-ui'
@@ -15,6 +15,10 @@ import { Message } from 'element-ui'
 //Vue.use()里可以是一个对象也可以是一个函数，如果是函数就会被当作install
 // 插件对象必须有install字段.install字段是一个函数.
 // 初始化插件对象需要通过Vue.use()，因为它会Vue.use直接调用了这个Install方法,
+
+//2019 3月份前的没有tiemline 时间线要手动引入
+import Timeline from './timeline/index'
+import TimelineItem from './timeline-item/index'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -51,6 +55,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 //一般插件会用USE导入 公共方法用prototype
 //Message需要全局挂载  $message是自己的一个自定义属性
 //每一个vue组件都是Vue的实例，所以组件内this可以拿到Vue.prototype上添加的属性和方法。
